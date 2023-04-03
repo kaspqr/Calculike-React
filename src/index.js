6ec04b4b6bfccff50c4a0884b876b35b2b753c1e
kaspqr <kasparvella@gmail.com>
2023-04-03 21:03:29 +0300

    homepage added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App';
 import './index.css';
-// import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-// if (process.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
